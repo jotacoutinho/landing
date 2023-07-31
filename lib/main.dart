97cbfa88a8ae4df4e67e836ab72d9efbbbedfce9
Jota <jpscoutinho94@gmail.com>
2023-07-31 01:17:24 -0300

    Fix info layout; Add OutlinedButton and Download section; Add logic to capture which device is running the app;

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:device_info/device_info.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,118 +34,118 @@ class MyHomePage extends StatelessWidget {
       height: double.infinity,
       child: Row(
         children: [
-          createInfoView(),
-          createImageView()
+          infoView(),
+          imageView()
         ]
       )
     );
   }
 
-  // Widgets as functions
-  // Widget createInfoView() { 
-  //   return Flexible(
-  //     child: FractionallySizedBox(
-  //       widthFactor: 0.5, 
-  //       child: Column(
-  //         children: [
-  //           bio,
-  //           // outlinedButton,
-  //           // socialContainer
-  //         ]
-  //       ),
-  //     ),
-  //   );
-  // }
+  // To adjust the layout depending on device running the app
+  Future<bool> _isRunningOnMobile() async {
+    try {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      
+      if(androidInfo.androidId.isNotEmpty || iosInfo.identifierForVendor.isNotEmpty) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      debugPrint('Error: $e');
+      // Defaults to mobile experience because it's less likely to break the layout
+      return true;
+    }
+  }
 
-  Widget createInfoView() { 
+  // Informational View
+  Widget infoView() { 
     return Expanded(
       flex: 1,
-      child: bio
+      child: bio()
     );
   }
 
-  Widget createImageView() { 
+  Widget bio() {
+    return Container(margin: const EdgeInsets.all(64.0), child: Wrap(
+      children: [
+        const Text(
+          "Hello. I'm an iOS Engineering Manager.",
+          style: TextStyle(
+            color: primaryColor,
+            fontSize: 32,
+            fontWeight: FontWeight.bold),
+          ),
+        const SizedBox(height: 100),
+        const Text(
+          "My name's João Pedro Coutinho. I manage a team of software engineers working remotely all over the world.",
+          style: TextStyle(
+            color: primaryColor,
+            fontSize: 20,
+            fontWeight: FontWeight.normal),
+          ),
+          const SizedBox(height: 150),
+          outlinedButton(),
+          const SizedBox(height: 150),
+          socialContainer(),
+      ],),
+    );
+  } 
+
+  Widget outlinedButton() { 
+    return OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            foregroundColor: primaryColor,
+            minimumSize: const Size(250, 50),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(25))),
+            side: const BorderSide(color: primaryColor)
+          ),
+          onPressed: () { 
+            // TODO: mailto
+          } ,
+          child: 
+            const Text("Let's work together!", style: TextStyle(fontSize: 18.0)),
+        );
+  }
+
+  Widget socialContainer() {
+    return Wrap(
+      children: [
+        Row(children: [
+          const Text("Download", style: TextStyle(color: Colors.white60, fontSize: 14, fontWeight: FontWeight.normal)),
+          TextButton(
+            onPressed: () {
+              // TODO: download PDF
+            },
+            child: const Text('my resume', style: 
+              TextStyle(
+                color: Colors.white, 
+                fontSize: 14, 
+                fontWeight: FontWeight.normal, 
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.white,
+                decorationThickness: 1.0))
+          ),
+          const Text("(PDF 1.08MB)", style: TextStyle(color: Colors.white60, fontSize: 14, fontWeight: FontWeight.normal)),
+        ],),
+      ],
+    );
+  } 
+
+  // Image View
+  Widget imageView() { 
     return Expanded(
       flex: 1,
       child: picture
     );
   }
 
-  // Widget createImageView() {
-  //   return const Flexible(
-  //     child: FractionallySizedBox(
-  //     widthFactor: 0.5,
-  //     heightFactor: 1,
-  //     child: Image(
-  //       image: backgroundImage,
-  //       fit: BoxFit.cover
-  //       ),
-  //     ),
-  //   );
-  // }
-
-// Widget as objects
-  Widget bio = Container(margin: EdgeInsets.all(64.0), child: Wrap(
-    children: const [
-      Text(
-        "Hello. I'm an iOS Engineering Manager.",
-        style: TextStyle(
-          color: primaryColor,
-          fontSize: 32,
-          fontWeight: FontWeight.bold),
-        ),
-      // FIXME: There must be a better way to do this spacing
-      SizedBox(
-        width: 100, 
-        height: 100,
-      ),
-      Text(
-        "My name's João Pedro Coutinho. I manage a team of engineers working remotely for many different clients worldwide.",
-        style: TextStyle(
-          color: primaryColor,
-          fontSize: 20,
-          fontWeight: FontWeight.normal),
-        ),
-    ],),
-  );
-
   Widget picture = const Image(
     image: backgroundImage,
     height: double.infinity,
     fit: BoxFit.fitHeight,
   );
-
-  Widget outlinedButton = OutlinedButton(
-    style: OutlinedButton.styleFrom(
-      foregroundColor: primaryColor,
-      minimumSize: const Size(88, 36),
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(18)),
-      ),
-    ),
-    onPressed: () { },
-    child: 
-      const Text('Contact me!'),
-    );
-
-    Widget socialContainer = Wrap(
-      children: [
-        Row(children: [
-          const Text(
-            "Download",
-            style: TextStyle(
-            color: primaryColor,
-            fontSize: 14,
-            fontWeight: FontWeight.normal),
-          ),
-          TextButton(
-            onPressed: () {} ,
-            child:
-              const Text('my resume'),
-            ),
-          ],
-        )
-      ],
-    );
 }
